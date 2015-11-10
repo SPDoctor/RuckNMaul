cff321e198504eda57bb228d04b7de4dea51430e
AzureAD\BillAyers <BillA@flosim.com>
2015-11-10 20:03:10 +0100

    added task lit

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -3,23 +3,33 @@
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
 (function () {
-    "use strict";
+	"use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-    };
+	function onDeviceReady() {
+		// Handle the Cordova pause and resume events
+		document.addEventListener('pause', onPause.bind(this), false);
+		document.addEventListener('resume', onResume.bind(this), false);
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+		// TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+	};
+
+	function onPause() {
+		// TODO: This application has been suspended. Save application state here.
+	};
+
+	function onResume() {
+		// TODO: This application has been reactivated. Restore application state here.
+	};
+
+	function log(error) {
+		document.getElementById("error").innerHTML = error;
+	}
+
+})();
+
+function load() {
+
+}
